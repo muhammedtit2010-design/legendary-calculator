c9dec9f4f7173b1498898b6d6a52a64f7755313e
muhammedtit2010-design <muhammedtit2010@gmail.com>
2026-08-15 14:49:27 +0300

    Add calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("=== الآلة الحاسبة ===")
+
+num1 = float(input("أدخل الرقم الأول: "))
+operator = input("أدخل العملية (+ - * /): ")
+num2 = float(input("أدخل الرقم الثاني: "))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "لا يمكن القسمة على صفر"
+else:
+    result = "عملية غير صحيحة"
+
+print("النتيجة:", result)
